be3ac4fce790f62c01f02acb5e54e63db219c9b2
bl-scrumbucket <bl.scrumbucket@gmail.com>
2018-02-19 22:21:57 -0500

    Adding first set of Data Objects

---
+++ ngOnCallSupport/src/app/dataObjects/employee.ts
@@ -0,0 +1,14 @@
+import { Team } from "./team";
+
+export class Employee {
+    constructor(
+        public id:string ="",
+        public firstName:string="",
+        public lastName:string="",
+        public phone:number=0,
+        public photoUrl:string="",
+        public teamId:string=""
+        //TODO : Talk to team if this will help
+        //public team:Team=new Team(),
+    ){}
+}
