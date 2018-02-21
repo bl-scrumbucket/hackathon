974353d00af501626cb70f4a436193a59d9be2ef
bl-scrumbucket <bl.scrumbucket@gmail.com>
2018-02-20 19:41:26 -0500

    Adding a service which will provide sample data

---
+++ ngOnCallSupport/src/app/crud-service.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class CrudServiceService {
+
+  constructor(private http:Http) { }
+
+  public getOnCallSnapshot(){
+    var sampleJson = {
+      "onCallList" : [{
+      "teamId" : "scrumBucket",
+      "teamName" : "Scrum Bucket",
+      "employeeId" : "smukunda",
+      "employeeName" : "Satish Mukundachar"
+    },{
+      "teamId" : "storesTeam",
+      "teamName" : "Stores Team",
+      "employeeId" : "nrevanap",
+      "employeeName" : "Nagaraja Revanappa"
+    }]
+    }
+    return sampleJson;
+  }
+
+
+  public getOnCallInfoForTeam(id:string){
+    var sampleJson = {
+      "teamId" : "scrumBucket",
+      "teamName" : "Scrum Bucket",
+      "employeeId" : "smukunda",
+      "employeeName" : "Satish Mukundachar",
+      "employeePhone" : "6142787271",
+      "escalation1Name" : "Doug Coning",
+      "escalation1Phone" : "612786757",
+      "escalation2Name" : "Ananda Jami",
+      "escalation2Phone" : "6142781234"
+    }
+    return sampleJson;
+  }
+}
